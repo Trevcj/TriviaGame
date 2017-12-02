05bc246fbaa6ca031071190359625ff7b77f2665
Trevor <trevcj@gmail.com>
2017-12-02 12:16:42 -0700

    update

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,3 @@
-var time = 30;
-
 var questions = [
  'In "The Fellowship of the Ring" what is the name of the ferry the hobbits use to escape the Black Riders?',
  'Where was the entire trilogy of the "Lord of the Rings" filmed?', 
@@ -14,8 +12,8 @@ var answers = [
  ["Bucklebury Ferry", "Raspberry Ferry", "Huckleberry Ferry", "Disney Cruise"],
  ["On a camera","New Zealand","Middle Earth","Peter Jackson's backyard"],
  ["The Grey Pilgrim", "Gandalf Greyhame", "The White Ranger", "Gandalf Stormcrow"], 
- ["Revenge of the Sith","Two Towers","Fellowship of the Rings","Shrek the Musical"], 
- ["Their moms met at the playground", "They met on ehobbity.com", "Sam and Frodo are brothers", "Sam is his gardner"], 
+ ["Revenge of the Sith","Two Towers","Fellowship of the Ring","Shrek the Musical"], 
+ ["Their moms met at the playground", "They met on ehobbity.com", "Sam and Frodo are brothers", "Sam is his gardener"], 
  ["Legolas","Prince Charming","The Ranger","Viserys Targaryen"], 
  ["Two horses", "A cat and a ferret", "A rat and a parrot", "Two prancing ponies"], 
  ["Sean Bean","Elijah Wood","Viggo Mortensen","Sean Astin"]];
@@ -35,7 +33,7 @@ var correctAnswers = [
 "B. New Zealand", 
 "C. The White Ranger", 
 "C. Fellowship of the Ring", 
-"D. Sam is his gardner", 
+"D. Sam is his gardener", 
 "A. Legolas", 
 "B. A cat and a ferret", 
 "D. Sean Astin"];
@@ -43,36 +41,41 @@ var correctAnswers = [
 var questionNumber = 0;
 var selecterAnswer;
 var clock;
+var time = 30;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 var win = 0;
 var loss = 0;
 var counter = 30;
+var layout;
 
 $("#start").on("click", function () {
 	qa();
+	$("#start").hide();
+	clockFun();
 });
 
 function qa(){
 
 	var layout = "<p>Time Remaining: <span class='timer'>30</span></p><p>" + questions[questionNumber] + "</p><p class='answers'>A. " + answers[questionNumber][0] + "</p><p class='answers'>B. "+answers[questionNumber][1]+"</p><p class='answers'>C. "+answers[questionNumber][2]+"</p><p class='answers'>D. "+answers[questionNumber][3]+"</p>";
 	$(".questions").html(layout);
+	
 }
 function wait(){
 
-	if (questionCounter < 7) {
+	if (questionNumber < 7) {
 	questionNumber++;
 	qa();
 	counter = 30;
 	}
 }
 
-function timerWrapper() {
-	Clock = setInterval(thirtySeconds, 1000);
+function clockFun() {
+	clock = setInterval(thirtySeconds, 1000);
 	function thirtySeconds() {
 		if (counter === 0) {
-			clearInterval(Clock);
+			clearInterval(clock);
 			lossToTime();
 		}
 		if (counter > 0) {
@@ -84,25 +87,33 @@ function timerWrapper() {
 
 function lossToTime(){
 
-	unanswered++;
-	setTimeout(wait, 3000);  //  change to 4000 or other amount
+	loss++;
+	setTimeout(wait, 3000);
+	$(".loss").html('<p>Losses:' + loss + ' </p>');
+	qa();
+	clearInterval(clock);
+	clockFun();
 }
 
 
-$(".answers").on("click", function (){
+$(".questions").on("click", ".answers", function (){
 	selectedAnswer = $(this).text();
 	console.log(selectedAnswer);
 	if(selectedAnswer === correctAnswers[questionNumber]) {
-		
-		clearInterval(clock);
 		win++;
 		$(".win").html('<p>Wins:' + win + ' </p>');
+		wait();
+		qa();
+		clearInterval(clock);
+		clockFun();
 	}
 	else {
-		//alert("wrong answer!");
-		clearInterval(clock);
 		loss++;
 		$(".loss").html('<p>Losses:' + loss + ' </p>');
+		wait();
+		qa();
+		clearInterval(clock);
+		clockFun();
 	}
 });
 
